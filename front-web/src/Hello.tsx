0312ea7210a568f1e9f4e49c255fdb3d1b98477e
Henrique Lins <henriquelins@msn.com>
2021-01-08 11:57:27 -0300

    Create front-web initial files

---
+++ front-web/src/Hello.tsx
@@ -0,0 +1,19 @@
+import { type } from "os";
+import { useEffect } from "react";
+
+type Props = {
+    message: string;
+}
+
+function Hello({message}: Props) {
+
+    useEffect( () => {
+        console.log('componente iniciou!')
+    }, [])
+
+    return (
+        <h1>Hello {message}!</h1>
+    )
+}
+
+export default Hello;
